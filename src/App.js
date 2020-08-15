a945854c7b43dca57466fec7a9f2a88a76008e9a
Jun Hashimoto <jun.hashimoto211@gmail.com>
2020-08-15 12:43:45 +1200

    adding useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('use effect');
+    }, [count])
 
     return (
         <>
