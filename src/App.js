d5d39ab0976463155b9bfa24d85cdc9e5529985b
Jun Hashimoto <jun.hashimoto211@gmail.com>
2020-08-16 09:25:36 +1200

    fixinb bad merge

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-<<<<<<< Updated upstream
-import React, { useState, useEffect } from 'react';
-import './App.css';
-=======
 import React, { useState } from "react";
 import { Timer } from './Timer';
->>>>>>> Stashed changes
 
-const App = () => {
-    const [count, setCount] = useState(0);
+const Child = React.memo(props => {
+    console.log("render Child");
+    return <p>Child: {props.cnt}</p>;
+});
 
-    useEffect(() => {
-        console.log('use effect');
-    }, [count])
+export default function App() {
+    console.log("render App");
+
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
 
     return (
         <>
-<<<<<<< Updated upstream
-            <p>You clicked {count} times</p>
-            <button
-                onClick={() => setCount(count + 1)}>
-                Click Me
-            </button>
-=======
             <Timer></Timer>
             <button onClick={() => setCount1(count1 + 1)}>countup App count</button>
             <button onClick={() => setCount2(count2 + 1)}>countup Child count</button>
@@ -30,9 +22,6 @@ const App = () => {
             </p>
             <Child
                 cnt={count2} />
->>>>>>> Stashed changes
         </>
-    )
+    );
 }
-
-export default App;
