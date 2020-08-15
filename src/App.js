cdb53cf42052181a614ca50665ca0cc455f95a47
Jun Hashimoto <jun.hashimoto211@gmail.com>
2020-08-15 15:08:59 +1200

    adding memo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from "react";
 
-const App = () => {
-    const [count, setCount] = useState(0);
+const Child = React.memo(props => {
+    console.log("render Child");
+    return <p>Child: {props.cnt}</p>;
+});
 
-    useEffect(() => {
-        console.log('use effect');
-    }, [count])
+export default function App() {
+    console.log("render App");
+
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
 
     return (
         <>
-            <p>You clicked {count} times</p>
-            <button
-                onClick={() => setCount(count + 1)}>
-                Click Me
-            </button>
+            <button onClick={() => setCount1(count1 + 1)}>countup App count</button>
+            <button onClick={() => setCount2(count2 + 1)}>countup Child count</button>
+            <p>
+                App: {count1}
+            </p>
+            <Child
+                cnt={count2} />
         </>
-    )
+    );
 }
-
-export default App;
